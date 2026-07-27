fa9a0439931f806f3050746ab7327c1371051385
Shrinet82 <shashwat.pratap94550@gmail.com>
2026-07-27 18:14:35 +0530

    fix: remove unused imports for deleted post and category schemas

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,6 +1,4 @@
 import author from "./schemas/author";
-import category from "./schemas/category";
-import post from "./schemas/post";
 import book from "./schemas/book";
 import journal from "./schemas/journal";
 import actLaw from "./schemas/actLaw";
